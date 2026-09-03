65df16ff21a81a5b51dfbb4788648dec44645820
godack <isaiasvelasotil@gmail.com>
2026-09-03 20:07:16 +0200

    Feat: view counter

---
+++ docs/javascripts/goatcounter.js
@@ -0,0 +1,7 @@
+(function () {
+  var script = document.createElement('script');
+  script.dataset.goatcounter = 'https://isaiasvela.goatcounter.com/count';
+  script.async = true;
+  script.src = 'https://gc.zgo.at/count.js';
+  document.head.appendChild(script);
+})();
